a0d11857d8c86dc3bd4c7c590afa5cbfbee115c3
Chris Homsey <chomsey1@gmail.com>
2018-04-05 10:02:26 -0400

    Update Document.cs

--- Document.cs
+++ Document.cs
@@ -9,13 +9,13 @@ public class Document
 		
         // Creates a new array to split the Borrower Name input String
         String[] NameSplit = new String[] {""};
-		String FullName = i.InputBorrowerName;
+	String FullName = i.InputBorrowerName;
         
         // Sets the Separator value to " "
-		String[] Separators = new String[] {" "};
+	String[] Separators = new String[] {" "};
 
         // Splits FullName into an array of each part of the name, and then grabs the last item in the Array as the BorrowerLastName
-		NameSplit = FullName.Split(Separators, StringSplitOptions.RemoveEmptyEntries);
-		o.BorrowerLastName = NameSplit[NameSplit.Length - 1].ToUpper();
+	NameSplit = FullName.Split(Separators, StringSplitOptions.RemoveEmptyEntries);
+	o.BorrowerLastName = NameSplit[NameSplit.Length - 1].ToUpper();
 	}
 }
